6faafcb0cb8df05d17568292e0eb700c8f8027c1
Norbert Zehetmayer <norbert.zehetmayer@tuwien.ac.at>
2025-05-10 15:18:56 +0200

    change typing and inf handling in beam_frequency

--- mcp-naturalfrequency.py
+++ mcp-naturalfrequency.py
@@ -5,7 +5,7 @@ from naturalfrequency import general_beam_natural_frequency
 mcp = FastMCP("beam_frequency")
 
 @mcp.tool()
-async def simple_beams_frquency(beamtype: str,rhoA: float,EI: 
+async def simple_beam_frquency(beamtype: str,rhoA: float,EI: 
                          float,length: float,) -> float:
     """Calcuate the natural angular frequency of a beam.
 
@@ -31,10 +31,11 @@ async def simple_beams_frquency(beamtype: str,rhoA: float,EI:
     else:
         raise ValueError("Invalid type. Choose from 'cantilever', " \
         "'simply_supported', 'popped cantilever', or 'fixed'.")
-    
-async def beams_frquency(rhoA: float,EI: float,length: float, 
-                         linearsprings: tuple[float,float],
-                         torsionalsprings: tuple[float,float]) -> float:
+
+@mcp.tool() 
+async def beam_frquency(rhoA: float,EI: float,length: float, 
+                         linearsprings: tuple[float | str, float | str],
+                         torsionalsprings: tuple[float | str, float | str]) -> float:
     """Calcuate the natural angular frequency of a beam,
     with linear and torsional springs at the beginning and end of the beam.
 
@@ -52,38 +53,48 @@ async def beams_frquency(rhoA: float,EI: float,length: float,
     K1, K2 = linearsprings
     C1, C2 = torsionalsprings
 
+    if isinstance(K1, str) and 'inf' in K1.lower():
+        K1=inf
+    if isinstance(K2, str) and 'inf' in K2.lower():
+        K2=inf
+    if isinstance(C1, str) and 'inf' in C1.lower():
+        C1=inf
+    if isinstance(C2, str) and 'inf' in C2.lower():
+        C2=inf
+
     k1 = K1*length**3/EI
     k2 = K2*length**3/EI
     c1 = C1*length/EI
     c2 = C2*length/EI
 
-    if k1 < 0:
+    
+    if k1 < 0.:
         raise ValueError("Invalid value for linear spring at the beginning. Choose from 0, inf or positive values.")
-    elif k1 < 1:
-        k_1 = [k1, 1]
+    elif k1 < 1.:
+        k_1 = (k1, 1.)
     else:
-        k_1 = [1, 1/k1]
+        k_1 = (1., 1/k1)
     #
-    if k2 < 0:
+    if k2 < 0.:
         raise ValueError("Invalid value for linear spring at the end. Choose from 0, inf or positive values.")
-    elif k2 < 1:
-        k_2 = [k2, 1]
+    elif k2 < 1.:
+        k_2 = (k2, 1.)
     else:
-        k_2 = [1, 1/k2]
+        k_2 = (1., 1/k2)
     #
-    if c1 < 0:
+    if c1 < 0.:
         raise ValueError("Invalid value for torsional spring at the beginning. Choose from 0, inf or positive values.")
-    elif c1 < 1:
-        c_1 = [c1, 1]
+    elif c1 < 1.:
+        c_1 = (c1, 1.)
     else:
-        c_1 = [1, 1/c1]
+        c_1 = (1., 1/c1)
     #
-    if c2 < 0:
+    if c2 < 0.:
         raise ValueError("Invalid value for torsional spring at the end. Choose from 0, inf or positive values.")
-    elif c2 < 1:
-        c_2 = [c2, 1]
+    elif c2 < 1.:
+        c_2 = (c2, 1.)
     else:
-        c_2 = [1, 1/c2]
+        c_2 = (1., 1/c2)
     
     eps = general_beam_natural_frequency(k_1,k_2,c_1,c_2)
 
